fc0600292c7e6b5074e72953d246917a4b2fc257
mephi007 <roysumit949@gmail.com>
2021-11-23 18:00:01 +0530

    My First React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Header } from "./components/Header";
+import { Todos } from "./components/Todos";
+import { Footer } from "./components/Footer";
+import React, { useState, useEffect } from "react";
+import { AddTodo } from "./components/AddTodo";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch } from "react-router";
+import { About } from "./components/About";
+
 
 function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+  const onDelete = (todo) => {
+    console.log("I will be deleted", todo);
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
+  };
+  const addTodo = (title, desc) => {
+    // console.log("adding new todo", title, desc)
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    } else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    };
+    setTodos([...todos, myTodo]);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+  // {
+  //   sno : 1,
+  //   title: "Market",
+  //   desc: "go to sabji market"
+  // },
+  // {
+  //   sno : 2,
+  //   title: "gym",
+  //   desc: "go to gym"
+  // },
+  // {
+  //   sno : 3,
+  //   title: "Basketball",
+  //   desc: "go to play basketball"
+  // },
+  // {
+  //   sno : 4,
+  //   title: "tuition",
+  //   desc: "go to tuition"
+  // },
+  // ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header title="My Todos List" searchBar={false} />
+        <Switch>
+          <Route
+            exact path="/"
+            render={() => {
+              return (
+                <>
+                  <AddTodo addTodo={addTodo} />
+                  <Todos todos={todos} onDelete={onDelete} />
+                </>
+              )
+            }}
+          >
+          </Route>
+          <Route path="/about">
+              <About />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+          This is an about Component.
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aperiam
+            cum voluptate, nihil adipisci natus tempora corporis quos ducimus
+            libero, modi dicta recusandae, in eveniet maiores deserunt. Iusto,
+            doloribus maxime!
+          </p>
+        </div>
+      );
+}
